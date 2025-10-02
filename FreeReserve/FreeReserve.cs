c611ebb36feb06fb43d529e5434e52117776b5ca
Guest0451F <152140863+Guest0451F@users.noreply.github.com>
2025-10-02 19:41:03 +0700

    Add files via upload

---
+++ FreeReserve/FreeReserve.cs
@@ -0,0 +1,53 @@
+﻿using HarmonyLib;
+using System;
+
+namespace RemoveReserveUpkeepMod
+{
+    public class RemoveReserveUpkeepMod : GameModification
+    {
+        Harmony _harmony;
+
+        public RemoveReserveUpkeepMod(Mod p_mod) : base(p_mod) { }
+
+        public override void OnModInitialization(Mod p_mod)
+        {
+            mod = p_mod;
+            PatchGame();
+        }
+
+        public override void OnModUnloaded()
+        {
+            _harmony?.UnpatchAll(_harmony.Id);
+        }
+
+        void PatchGame()
+        {
+            _harmony = new Harmony("com.hexofsteel.removereserveupkeep");
+            _harmony.PatchAll();
+        }
+    }
+
+    [HarmonyPatch(typeof(Unit))]
+    [HarmonyPatch(nameof(Unit.GetUpkeepCost))]
+    public static class RemoveReserveUpkeepPatch
+    {
+        [HarmonyPrefix]
+        static bool Prefix(Unit __instance, bool p_getReserveCost, ref int __result)
+        {
+            try
+            {
+                if (p_getReserveCost && __instance.IsReserve)
+                {
+                    __result = 0;
+                    return false;
+                }
+                
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return true;
+            }
+        }
+    }
+}
